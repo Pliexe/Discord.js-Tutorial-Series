c1358cad540e7e1306b73e8a0d3728a2346d8eb6
László-zsolt Szabadi <42877326+Pliexe@users.noreply.github.com>
2020-12-03 06:33:42 +0100

    Episode 2 code uploaded

---
+++ EPISODE 2/index.js
@@ -0,0 +1,22 @@
+const { Client } = require('discord.js');
+const config = require('./config.json');
+
+const bot = new Client({ disableMentions: "everyone" });
+
+bot.on('ready', () => {
+    console.log("Ready!");
+});
+
+bot.on('message', message => {
+    if (message.author.bot) return;
+
+    let [command, ...args] = message.content.slice(config.PREFIX.length).split(/ +/g);
+
+    switch (command.toLowerCase()) {
+        case "ping":
+            message.channel.send("Pong!");
+            break;
+    }
+});
+
+bot.login(config.TOKEN);
